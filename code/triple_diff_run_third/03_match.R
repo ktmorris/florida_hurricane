04a9fa970580d9b08137676217e373749116d120
Kevin <kevin.morris@nyu.edu>
2020-02-26 13:07:48 -0500

    set up triple diff

---
+++ code/triple_diff_run_third/03_match.R
@@ -0,0 +1,42 @@
+## this can be run locally or on NYU's HPC. Set option in next step
+## option allowed because of how long GenMatch can take
+
+on_nyu <- F
+if(on_nyu){
+  library(Matching)
+  library(data.table)
+  library(snow)
+  library(parallel)
+  library(scales)
+  library(kableExtra)
+  library(tidyverse)
+  
+  setwd("/scratch/km3815/matching")
+}
+
+
+
+#####
+fl_roll <- readRDS("./temp/pre_match_full_voters.rds") %>% 
+  filter(!treated) %>%
+  mutate(treated = neighbor_county_match) %>%
+  select(-neighbor_county_match) %>%
+  filter((!neighbor_county | treated))
+
+##########
+
+ids <- fl_roll %>% 
+  mutate(id = row_number()) %>% 
+  select(id, LALVOTERID)
+
+X = fl_roll %>%
+  dplyr::select(white, black, latino, asian, female, male, dem, rep, age,
+                median_income, some_college)
+
+
+genout <- readRDS("./temp/genout_hurricane.rds")
+
+mout <- Match(Tr = fl_roll$treated, X = X,
+                estimand = "ATT", Weight.matrix = genout, M = 5)
+
+save(mout, file = "./temp/mout_hurricane_full.RData")
